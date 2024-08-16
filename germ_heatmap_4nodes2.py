22c7dcd89bdcf6a910725bf5620f84e9d798fec1
Sun Haoran <151923611+kevin-kas@users.noreply.github.com>
2024-08-16 11:28:21 +0800

    Add files via upload

---
+++ germ_heatmap_4nodes2.py
@@ -0,0 +1,184 @@
+from collections import Counter
+#the list has been selected in according to the mean heatmap
+UC_germs=['1650663', '1965588', '745368', '301302', '1965581', '1891969', '1965549', '1965550', '1965639', '1834105', '622312', '411483', '1965655', '411471', '1965648', '1965551']
+CD_germs=['1965631', '1965588', '1965581', '1965639', '762967', '1965551', '1834196', '537011', '1834112', '745368', '1891970', '1965650', '1965655', '411471', '1776047', '1834088', '1852381', '1531', '997894', '301302', '1203554', '1891969', '1776046', '1965549', '1965550', '411483', '2585118', '622312', '1650663', '1834105', '536231', '1965648', '1574262', '1033732']
+non_germs=['1776045', '1965588', '1965581', '1834109', '1965639', '53378', '1965551', '537011', '665953', '745368', '1891970', '81858', '411471', '1776047', '1834088', '1891969', '1776046', '1965550', '411483', '449673', '46503', '1965648']
+
+all_germs=[]
+all_germs.extend(UC_germs)
+all_germs.extend(CD_germs)
+all_germs.extend(non_germs)
+all_germs=sorted(list(set(all_germs)))
+print(all_germs)
+def matcher(str1):
+	matcher1=re.match(r'\(\'([^\']*)\',\s*(\d+)',str1)
+	float_str=matcher1.group(1)
+	integer_str=matcher1.group(2)
+	return float_str,integer_str
+
+sicks=['UC','CD','non']
+
+import os,csv,re
+import warnings
+warnings.filterwarnings('ignore')
+
+def toconnect(label,list1):
+    new_list1=[]
+    for i in list1:
+        germs=label.split(',')
+        for j in range(len(i)):
+            if i[j]==0:
+                continue
+            else:
+                if j==0:
+                    for n in range(i[j]):
+                        new_list1.append((germs[0],germs[1]))
+                elif j==1:
+                    for n in range(i[j]):
+                        new_list1.append((germs[0],germs[2]))
+                elif j==2:
+                    for n in range(i[j]):
+                        new_list1.append((germs[0],germs[3]))
+                elif j==3:
+                    for n in range(i[j]):
+                        new_list1.append((germs[1],germs[2]))
+                elif j==4:
+                    for n in range(i[j]):
+                        new_list1.append((germs[1],germs[3]))
+                elif j==5:
+                    for n in range(i[j]):
+                        new_list1.append((germs[2],germs[3]))
+    return new_list1
+
+all_structure=[]
+all_nodes=[]
+all_patient_info=[]
+all_info=dict()
+
+for sick in  sicks:
+	for patient_name in os.listdir('IBD_DetailedCSV_15_4'):
+		if patient_name.split('_')[1]!=sick:
+			continue
+		table_folder=os.path.join('IBD_DetailedCSV_15_4', patient_name)
+		for info_table in os.listdir(table_folder):
+			with open('IBD_DetailedCSV_15_4/'+patient_name+'/'+info_table) as f:
+				reader=csv.reader(f)
+				dict1=dict()
+				for lines in reader:
+					if lines==[]:
+						continue
+					struture: list[list[int]]=[]
+					for string_idx in range(1,len(lines)):
+						float_str,integer=matcher(lines[string_idx])
+						edge_value_list=list(map(lambda x:int(float(x)),float_str.split(' ')))
+						for i in range(int(integer)):
+							struture.append(edge_value_list)
+					dict1[lines[0]]=toconnect(lines[0],struture)
+
+			for node_comb in dict1.keys():
+				all_nodes.append(node_comb)
+			all_patient_info.append(dict1)
+
+			if patient_name not in all_info.keys():
+				all_info[patient_name]=[]
+			all_info[patient_name].append(dict1)
+
+import pandas as pd
+#检验每一种细菌的计数
+UC_germs_df=pd.DataFrame(columns=all_germs,index=all_germs[::-1])
+CD_germs_df=pd.DataFrame(columns=all_germs,index=all_germs[::-1])
+non_germs_df=pd.DataFrame(columns=all_germs,index=all_germs[::-1])
+
+all_info2_non=[]
+for i in all_info.keys():
+    label=i.split('_')[1]
+    if label!='non':
+        continue
+    for dict1 in all_info[i]:
+        a=dict1.values()
+        for n in a:
+            all_info2_non.extend(n)
+
+all_info2_UC=[]
+for i in all_info.keys():
+    label=i.split('_')[1]
+    if label!='UC':
+        continue
+    for dict1 in all_info[i]:
+        a=dict1.values()
+        for n in a:
+            all_info2_UC.extend(n)
+
+all_info2_CD=[]
+for i in all_info.keys():
+    label=i.split('_')[1]
+    if label!='CD':
+        continue
+    for dict1 in all_info[i]:
+        a=dict1.values()
+        for n in a:
+            all_info2_CD.extend(n)
+
+counter_non=Counter(all_info2_non)
+counter_UC=Counter(all_info2_UC)
+counter_CD=Counter(all_info2_CD)
+
+for i in UC_germs_df.columns:
+    for j in UC_germs_df.index:
+        if i==j:
+            continue
+        else:
+            UC_germs_df.loc[j,i]=counter_UC[(i,j)]
+            UC_germs_df.loc[i,j]=counter_UC[(i,j)]
+
+for i in CD_germs_df.columns:
+    for j in CD_germs_df.index:
+        if i==j:
+            continue
+        else:
+            CD_germs_df.loc[j,i]=counter_CD[(i,j)]
+            CD_germs_df.loc[i,j]=counter_CD[(i,j)]
+
+
+for i in non_germs_df.columns:
+    for j in non_germs_df.index:
+        if i==j:
+            continue
+        else:
+            non_germs_df.loc[j,i]=counter_non[(i,j)]
+            non_germs_df.loc[i,j]=counter_non[(i,j)]
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+UC_germs_df =UC_germs_df.apply(pd.to_numeric, errors='coerce')
+UC_germs_df.fillna(0,inplace=True)
+CD_germs_df=CD_germs_df.apply(pd.to_numeric, errors='coerce')
+CD_germs_df.fillna(0,inplace=True)
+non_germs_df=non_germs_df.apply(pd.to_numeric, errors='coerce')
+non_germs_df.fillna(0,inplace=True)
+
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.cm import ScalarMappable
+def toone(list1):
+	a=[]
+	for i in list1:
+		list2=[]
+		for j in i:
+			list2.append(j/255)
+		a.append(tuple(list2))
+	return a
+
+colors = [(240,240,240),(76,164,162),(54,104,107),(25,49,52)]
+colors=toone(colors)
+cmap_name = 'my_custom_cmap'
+cmap = LinearSegmentedColormap.from_list(cmap_name, colors)
+sm = ScalarMappable(cmap=cmap)
+middle_value = (CD_germs_df.max() - CD_germs_df.min()) / 2
+color = sm.to_rgba(middle_value)
+
+plt.figure(figsize=(20,20))
+plt.gcf().autofmt_xdate()
+plt.title('CD_germs with 4 nodes')
+sns.heatmap(CD_germs_df, annot=False, fmt=".2f", cmap=cmap)
+plt.show()
+print(non_germs_df)
